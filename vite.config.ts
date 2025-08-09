4a0253426ffa65ef831c1a7e543cca23922af0e9
Rawey Lugo <rawey08@gmail.com>
2025-08-09 17:34:10 -0400

    Fix vite build output and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  // ...
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "client", "dist"),
     emptyOutDir: true,
   },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
     },
   },
+  plugins: [react()],
 });
